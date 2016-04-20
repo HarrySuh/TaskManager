bf4c866e72c92de4bb5806ab750c7b15c3ae4b6f
HarrySuh <HarryJ.Suh99@gmail.com>
2016-04-20 10:07:45 -0700

    a

---
+++ app/src/main/java/com/example/csastudent2015/taskapp/TaskFragment.java
@@ -0,0 +1,19 @@
+package com.example.csastudent2015.taskapp;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by csastudent2015 on 4/14/16.
+ */
+public class TaskFragment extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View rootView = inflater.inflate(R.layout.fragment_task, container, false);
+        return rootView;
+    }
+}
