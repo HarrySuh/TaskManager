483fa5a67eaf2516016f93787375e6c55b4045c0
chrischan53 <chrischan53@gmail.com>
2016-05-31 09:03:58 -0700

    taskActivity & taskActivityFragment

--- app/src/main/java/com/example/csastudent2015/taskapp/TaskFragment.java
+++ app/src/main/java/com/example/csastudent2015/taskapp/TaskFragment.java
@@ -5,18 +5,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
 
 /**
  * Created by csastudent2015 on 4/14/16.
  */
 public class TaskFragment extends Fragment {
-    private EditText taskName;
-    private EditText taskDescription;
-    private String text;
-    private Button create;
-
 
 
     public void onCreate(Bundle savedInstanceState) {
@@ -26,7 +19,6 @@ public class TaskFragment extends Fragment {
     @Override
     public void onSaveInstanceState(Bundle state) {
         super.onSaveInstanceState(state);
-        state.putCharSequence(text, taskName.getText().toString());
     }
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -35,34 +27,9 @@ public class TaskFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.activity_task, container, false);
 
 
-        taskName = (EditText) rootView.findViewById(R.id.taskName);
-        taskDescription = (EditText) rootView.findViewById(R.id.taskDescription);
-        create = (Button) rootView.findViewById(R.id.createButton);
-
-        if(savedInstanceState != null) {
-            taskName.setText(savedInstanceState.getCharSequence(text));
-            taskDescription.setText(savedInstanceState.getCharSequence(text));
-        }
-
-        create.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Do something in response to button click
-
-
-
-                }
-        });
-
         return rootView;
     }
-
-    public String getTaskName()
-    {
-        return taskName.toString();
-    }
-
-    public String getTaskDescription()
-    {
-        return taskDescription.toString();
-    }
 }
+
+
+
